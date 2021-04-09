346bb620aa5d4edb967ae652d1e2ac15cb8bd6d5
Lukas <lukas.weidich@gmx.de>
2021-04-09 10:47:45 +0200

    💄 add maps link and code image

--- components/index/Introduction.js
+++ components/index/Introduction.js
@@ -4,7 +4,7 @@ import { XTRA_LARGE_GAP } from "../../src/constants";
 import Container from "../layout/Container";
 import joyce from "../../assets/images/joyce_landing.jpg";
 import minden from "../../assets/images/minden.jpg";
-import team from "../../assets/images/team.jpg";
+import code from "../../assets/images/code.jpg";
 import IntroductionSection from "./IntroductionSection";
 
 const Introduction = () => {
@@ -16,20 +16,20 @@ const Introduction = () => {
 						src={joyce}
 						title="Ihr Partner"
 						content="QUIKK Software aus Minden konzipiert und entwickelt individuelle Softwarelösungen und Webdesigns nach Ihren Vorstellungen. Unseren Fokus setzen wir dabei auf agile Softwareentwicklung, eine transparente Kommunikation und eine strukturierte Arbeitsweise, um Ihnen sauberes und wartbares Coding zu liefern. Ohne Wenn und Aber."
-						alt="QUIKK Software Geschäftsführer und Ansprechpartner Joyce Rafflenbeul"
+						alt="QUIKK Software Minden Geschäftsführer Joyce Marvin Rafflenbeul"
 					/>
 					<IntroductionSection
 						src={minden}
 						title="In Minden"
 						content="Unser Büro liegt im wunderschönen Minden an der Weser in Ostwestfalen. In der Vergangenheit haben wir bereits viele Kunden aus der Region Hannover, Bielefeld und Osnabrück unterstützt. Sie sind weiter entfernt? Dank digitaler Absprachen und Remote Work können wir auch trotz großer Distanzen erfolgreich zusammenarbeiten."
 						revert={true}
-						alt="Minden (Ostwestfalen): QUIKK Software Standort"
+						alt="Minden (Ostwestfalen), NRW: QUIKK Software Standort Hahler Straße 285, 32427 Minden"
 					/>
 					<IntroductionSection
-						src={team}
+						src={code}
 						title="Mit Erfahrung"
 						content="Zunächst als Einzelunternehmer, seit Oktober 2020 im Team: Unser Geschäftsführer Joyce startet gemeinsam mit weiteren studierten Informatikern voll durch und kann auf mehrjährige Erfahrung in der Web- und Anwendungsentwicklung zurückgreifen. Wir sind Ihr Ansprechpartner für agile Softwareentwicklung und Webdesign in Minden."
-						alt="QUIKK Software Team"
+						alt="QUIKK Software Minden: Code, Softwareentwicklung und Webdesign"
 					/>
 				</Grid.Container>
 			</Container>
--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,10 +1,10 @@
 import {
-  Description,
-  Divider,
-  GeistProvider,
-  Grid,
-  Text,
-  useTheme,
+	Description,
+	Divider,
+	GeistProvider,
+	Grid,
+	Text,
+	useTheme,
 } from "@geist-ui/react";
 import React from "react";
 import { MEDIUM_GAP } from "../../src/constants";
@@ -15,87 +15,90 @@ import ContactLinks from "./footer/ContactLinks";
 import CompanyLinks from "./footer/CompanyLinks";
 import SocialLinks from "./footer/SocialLinks";
 import { QUIKK_dark, QUIKK_light } from "../../styles/themes";
+import ExternalLink from "../misc/ExternalLink";
 
 const Footer = () => {
-  const theme = useTheme();
+	const theme = useTheme();
 
-  const SanitizeHeading = ({ title = "" }) => (
-    <Text b style={{ color: theme.palette.accents_3 }}>
-      {title}
-    </Text>
-  );
-  const SanitizeContent = ({ children }) => (
-    <Text style={{ fontWeight: "normal", color: theme.palette.background }}>
-      {children}
-    </Text>
-  );
+	const SanitizeHeading = ({ title = "" }) => (
+		<Text b style={{ color: theme.palette.accents_3 }}>
+			{title}
+		</Text>
+	);
+	const SanitizeContent = ({ children }) => (
+		<Text style={{ fontWeight: "normal", color: theme.palette.background }}>
+			{children}
+		</Text>
+	);
 
-  const content = [
-    <Description
-      title={<SanitizeHeading title="Rechtliches" />}
-      content={
-        <SanitizeContent>
-          <LegalLinks />
-        </SanitizeContent>
-      }
-    />,
-    <Description
-      title={<SanitizeHeading title="Kontakt" />}
-      content={
-        <SanitizeContent>
-          <ContactLinks />
-        </SanitizeContent>
-      }
-    />,
-    <Description
-      title={<SanitizeHeading title="Unternehmen" />}
-      content={
-        <SanitizeContent>
-          <CompanyLinks />
-        </SanitizeContent>
-      }
-    />,
-    <Description
-      title={<SanitizeHeading title="Social" />}
-      content={
-        <SanitizeContent>
-          <SocialLinks />
-        </SanitizeContent>
-      }
-    />,
-    <Description
-      title={<SanitizeHeading title="Vor Ort" />}
-      content={
-        <SanitizeContent>
-          <LocationLinks />
-        </SanitizeContent>
-      }
-    />,
-  ];
+	const content = [
+		<Description
+			title={<SanitizeHeading title="Rechtliches" />}
+			content={
+				<SanitizeContent>
+					<LegalLinks />
+				</SanitizeContent>
+			}
+		/>,
+		<Description
+			title={<SanitizeHeading title="Kontakt" />}
+			content={
+				<SanitizeContent>
+					<ContactLinks />
+				</SanitizeContent>
+			}
+		/>,
+		<Description
+			title={<SanitizeHeading title="Unternehmen" />}
+			content={
+				<SanitizeContent>
+					<CompanyLinks />
+				</SanitizeContent>
+			}
+		/>,
+		<Description
+			title={<SanitizeHeading title="Social" />}
+			content={
+				<SanitizeContent>
+					<SocialLinks />
+				</SanitizeContent>
+			}
+		/>,
+		<Description
+			title={<SanitizeHeading title="Vor Ort" />}
+			content={
+				<SanitizeContent>
+					<ExternalLink href="https://g.page/quikk?share">
+						<LocationLinks />
+					</ExternalLink>
+				</SanitizeContent>
+			}
+		/>,
+	];
 
-  return (
-    <GeistProvider themes={[QUIKK_light, QUIKK_dark]} themeType="QUIKK_dark">
-      <footer style={{ background: theme.palette.foreground }}>
-        <Container spacing>
-          <Grid.Container gap={MEDIUM_GAP}>
-            {content.map((_, i) => (
-              <Grid key={i} xs={12} sm={12} md={24 / content.length}>
-                {_}
-              </Grid>
-            ))}
-          </Grid.Container>
-          <Divider />
-          <Description
-            title={
-              <SanitizeHeading
-                title={`© 2020-${new Date().getFullYear()} QUIKK Software`}
-              />
-            }
-          />
-        </Container>
-      </footer>
-    </GeistProvider>
-  );
+	return (
+		<GeistProvider themes={[QUIKK_light, QUIKK_dark]} themeType="QUIKK_dark">
+			<footer style={{ background: theme.palette.foreground }}>
+				<Container spacing>
+					<Grid.Container gap={MEDIUM_GAP}>
+						{content.map((_, i) => (
+							<Grid key={i} xs={12} sm={12} md={24 / content.length}>
+								{_}
+							</Grid>
+						))}
+					</Grid.Container>
+					<Divider />
+					<Description
+						title={
+							<SanitizeHeading
+								title={`© 2020-${new Date().getFullYear()} QUIKK Software`}
+							/>
+						}
+					/>
+				</Container>
+			</footer>
+		</GeistProvider>
+	);
 };
 
 export default Footer;
