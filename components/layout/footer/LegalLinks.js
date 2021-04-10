d857877362826f19973725559f1a6f4ec9b8d718
Lukas <lukas.weidich@gmx.de>
2021-04-10 11:54:57 +0200

    ♻️ shorten legal notice term in footer

--- components/layout/footer/LegalLinks.js
+++ components/layout/footer/LegalLinks.js
@@ -3,16 +3,14 @@ import InternalLink from "../../misc/InternalLink";
 import Links from "./Links";
 
 const LegalLinks = () => {
-  return (
-    <Links
-      content={[
-        <InternalLink href="/impressum">Impressum</InternalLink>,
-        <InternalLink href="/datenschutzerklaerung">
-          Datenschutzerklärung
-        </InternalLink>,
-      ]}
-    />
-  );
+	return (
+		<Links
+			content={[
+				<InternalLink href="/impressum">Impressum</InternalLink>,
+				<InternalLink href="/datenschutzerklaerung">Datenschutz</InternalLink>,
+			]}
+		/>
+	);
 };
 
 export default LegalLinks;
