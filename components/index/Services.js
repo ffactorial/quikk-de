0103a8b61527b04741295491a283d9eee882596f
Lukas <lukas.weidich@gmx.de>
2021-04-13 08:46:40 +0200

    ♻️ add ecommerce as service

--- components/index/Services.js
+++ components/index/Services.js
@@ -4,94 +4,94 @@ import { XTRA_LARGE_GAP } from "../../src/constants";
 import Container from "../layout/Container";
 import ServiceCard from "./ServiceCard";
 import {
-  Layout,
-  Search,
-  Server,
-  Smartphone,
-  Code,
-  LogIn,
-  Grid as GridIcon,
-  FastForward,
-  Compass,
+	Layout,
+	Search,
+	Server,
+	Smartphone,
+	Code,
+	ShoppingBag,
+	Grid as GridIcon,
+	FastForward,
+	Compass,
 } from "@geist-ui/react-icons";
 import Title from "../misc/Title";
 
 const Services = () => {
-  const theme = useTheme();
+	const theme = useTheme();
 
-  const services = [
-    {
-      title: "Software",
-      content:
-        "Maßgeschneiderte Softwarelösungen als Desktop- und Webanwendung.",
-      icon: <Code />,
-    },
-    {
-      title: "Fullstack",
-      content: "Anwendungen mit Nutzeroberfläche, Server und Datenbank.",
-      icon: <Server />,
-    },
-    {
-      title: "Mobile Apps",
-      content: "Software für die Hosentasche. Mobile Apps für Android und iOS.",
-      icon: <Smartphone />,
-    },
-    {
-      title: "APIs",
-      content: "Schnittstellen für eine einfache Kommunikation Ihrer Systeme.",
-      icon: <GridIcon />,
-    },
-    {
-      title: "Integration",
-      content:
-        "Automatisiertes Verbinden von Anwendungen spart Zeit und Nerven.",
-      icon: <LogIn />,
-    },
-    {
-      title: "Prototyping",
-      content:
-        "Wir überprüfen und bestätigen die Durchführbarkeit Ihres Vorhabens.",
-      icon: <Compass />,
-    },
-    {
-      title: "Webdesign",
-      content: "Ein Webauftritt, der genauso einzigartig ist, wie Sie selbst.",
-      icon: <Layout />,
-    },
-    {
-      title: "SEO",
-      content:
-        "Lassen Sie sich in den Weiten des World Wide Webs besser finden.",
-      icon: <Search />,
-    },
-    {
-      title: "Agilität",
-      content:
-        "Erste Ergebnisse liegen bereits nach einem 2-wöchigen Sprint vor.",
-      icon: <FastForward />,
-    },
-  ];
+	const services = [
+		{
+			title: "Software",
+			content:
+				"Maßgeschneiderte Softwarelösungen als Desktop- und Webanwendung.",
+			icon: <Code />,
+		},
+		{
+			title: "Fullstack",
+			content: "Anwendungen mit Nutzeroberfläche, Server und Datenbank.",
+			icon: <Server />,
+		},
+		{
+			title: "Mobile Apps",
+			content: "Software für die Hosentasche. Mobile Apps für Android und iOS.",
+			icon: <Smartphone />,
+		},
+		{
+			title: "APIs",
+			content: "Schnittstellen für eine einfache Kommunikation Ihrer Systeme.",
+			icon: <GridIcon />,
+		},
+		{
+			title: "E-Commerce",
+			content:
+				"Eigene Bestell- und Buchungssysteme sowie eBay/mobile.de Anbindung.",
+			icon: <ShoppingBag />,
+		},
+		{
+			title: "Prototyping",
+			content:
+				"Wir überprüfen und bestätigen die Durchführbarkeit Ihres Vorhabens.",
+			icon: <Compass />,
+		},
+		{
+			title: "Webdesign",
+			content: "Ein Webauftritt, der genauso einzigartig ist, wie Sie selbst.",
+			icon: <Layout />,
+		},
+		{
+			title: "SEO",
+			content:
+				"Lassen Sie sich in den Weiten des World Wide Webs besser finden.",
+			icon: <Search />,
+		},
+		{
+			title: "Agilität",
+			content:
+				"Erste Ergebnisse liegen bereits nach einem 2-wöchigen Sprint vor.",
+			icon: <FastForward />,
+		},
+	];
 
-  return (
-    <section
-      style={{
-        background: theme.palette.accents_1,
-        borderTop: `1px solid ${theme.palette.accents_2}`,
-        borderBottom: `1px solid ${theme.palette.accents_2}`,
-      }}
-    >
-      <Container spacing>
-        <Grid.Container gap={XTRA_LARGE_GAP} alignItems="center">
-          <Grid xs={24}>
-            <Title title="Unser Fokus" />
-          </Grid>
-          {services.map((service, i) => (
-            <ServiceCard key={i} {...service} />
-          ))}
-        </Grid.Container>
-      </Container>
-    </section>
-  );
+	return (
+		<section
+			style={{
+				background: theme.palette.accents_1,
+				borderTop: `1px solid ${theme.palette.accents_2}`,
+				borderBottom: `1px solid ${theme.palette.accents_2}`,
+			}}
+		>
+			<Container spacing>
+				<Grid.Container gap={XTRA_LARGE_GAP} alignItems="center">
+					<Grid xs={24}>
+						<Title title="Unser Fokus" />
+					</Grid>
+					{services.map((service, i) => (
+						<ServiceCard key={i} {...service} />
+					))}
+				</Grid.Container>
+			</Container>
+		</section>
+	);
 };
 
 export default Services;
